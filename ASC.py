dab3946f1d533189562ba077e1ed0c477b18eeb4
Ab Kein <rickbatra0z@gmail.com>
2022-06-23 05:26:52 +0300

    add license

--- ASC.py
+++ ASC.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3.8
 # -*- coding: utf-8 -*-
 
+# Copyright (C) 2022 Perevoshchikov Egor (rickbatra0z@gmail.com)
+#
+# This file is part of ASC.
+#
+# ASC is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ASC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ASC.  If not, see <http://www.gnu.org/licenses/>.
+
+
 """Absorption spectrum calculator (ASC) based on correlation technique."""
 
 
